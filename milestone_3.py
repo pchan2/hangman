9fbd279b1be97bb99c3f10f109801de04b943e64
Patrick Chan <q_p18@hotmail.com>
2023-09-20 15:40:14 +0100

    Task2: Check whether the guess is in the word.

--- milestone_3.py
+++ milestone_3.py
@@ -1,8 +1,23 @@
 from curses.ascii import isalpha
 
+import random
+
+word_list = ['banana', 'grapes', 'watermelon', 'blueberry', 'carambola']
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = ''
+
 while True:
     guess = input('Guess a letter: ')
     if len(guess) and isalpha(guess):
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
