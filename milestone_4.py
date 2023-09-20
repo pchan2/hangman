7c3289828cfb92ff1051451285eb28aa71d456e5
Patrick Chan <q_p18@hotmail.com>
2023-09-21 00:13:37 +0100

    Task 7: Refactor and optimise current code.

--- milestone_4.py
+++ milestone_4.py
@@ -3,45 +3,67 @@ import random
 
 
 class Hangman:
+    '''
+    This class is used to represent a hangman.
 
+    Attributes:
+        _word_list (list): the list of words.
+        _num_lives (int): the number of lives with a default value of five.
+        _word (str): the word is randomly selected from _word_list.
+        word_guessed (list): the word guessed is a list of letters guessed with the initial values of '_'.
+        num_letters (int): the number of unique letters in the word that have not been guessed yet.
+        list_of_guesses (list): the list of letters guessed.
+    '''
     def __init__(self, word_list: list, num_lives: int=5) -> None:
-        self.word_list = word_list
-        self.num_lives = num_lives
-        self.word = random.choice(word_list)
-        self.word_guessed = self.initialise_word_guessed() # Characters guessed. ['_', '_', '_']
-        self.num_letters = len(set([*self.word])) # The number of UNIQUE letters in the word that have not been guessed yet.
-        self.list_of_guesses = [] # The list of characters guessed.
-
-    def initialise_word_guessed(self) -> list:
-        char_list = []
-        for _ in self.word:
-            char_list.append('_')
-        return char_list
-
-    def check_guess(self, guess):
-        guess = guess.lower()
-        if guess in self.word:
-            print(f"Good guess! {guess} is in the word.")
-            for i in range(len(self.word)):
-                if self.word[i] == guess:
-                    self.word_guessed[i] = guess
-            self.num_letters -= 1
-        else:
-            self.num_lives -= 1
-            print(f"Sorry, {guess} is not in the word.")
-            print(f"You have {self.num_lives} lives left.")
+        '''
+        See help(Hangman) for accurate signature.
+        '''
+        self._word_list = word_list
+        self._num_lives = num_lives
+        self._word = random.choice(word_list)
+        self.word_guessed = ['_'] * len(self._word)
+        self.num_letters = len(set([*self._word]))
+        self.list_of_guesses = [] 
     
-    def ask_for_input(self):
+    def ask_for_input(self) -> None:
+        '''
+        This function is used to ask for input from the user.
+
+        The user will be prompted continuously to guess a letter
+        until a valid input is entered.
+        '''
         while True:
             guess = input("Guess a letter: ")
-            if len(guess) != 1 or len(guess) == 1 and not isalpha(guess):
-                print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self.list_of_guesses:
-                print("You already tried that letter!")
-            else:
-                self.check_guess(guess)
+            if self.__is_valid_input(guess):
+                self.__check_guess(guess)
                 self.list_of_guesses.append(guess)
 
+    def __is_valid_input(self, guess: str) -> bool:
+        list_of_guesses = self.list_of_guesses
+        if len(guess) != 1 or len(guess) == 1 and not isalpha(guess):
+            print("Invalid letter. Please, enter a single " +
+                  "alphabetical character.")
+        elif guess in list_of_guesses:
+            print("You already tried that letter!")
+        else:
+            return True
+
+    def __check_guess(self, guess: str) -> None:
+        guess = guess.lower()
+        word = self._word
+        if guess in word:
+            print(f"Good guess! {guess} is in the word.")
+            self.__update_word_guessed(word, guess)
+            self.num_letters -= 1
+        else:
+            self._num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self._num_lives} lives left.")
+    
+    def __update_word_guessed(self, word: str, guess: str) -> None:
+        for i in range(len(word)):
+            if word[i] == guess:
+                self.word_guessed[i] = guess
 
 
 hangman = Hangman(['apple', 'pear', 'banana', 'kiwi', 'grapes'])
