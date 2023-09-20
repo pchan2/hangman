334c8ea70bc36511361268929097f23bf7310fd3
Patrick Chan <q_p18@hotmail.com>
2023-09-20 13:42:26 +0100

    Task 4: Check the input is a single character.

--- milestone_2.py
+++ milestone_2.py
@@ -7,3 +7,9 @@ word = random.choice(word_list)
 print(word)
 
 guess = input('enter a single letter: ')
+guess = guess.lower()
+print(guess)
+if len(guess) == 1 and 97 <= ord(guess) <= 122:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
