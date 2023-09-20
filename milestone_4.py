9af9f57d36b0f9b0445319fe087162006bdf98f3
Patrick Chan <q_p18@hotmail.com>
2023-09-20 22:17:50 +0100

    Debug for loop in check_guess() of Task 3: Define what happens if the letter is in the word.

--- milestone_4.py
+++ milestone_4.py
@@ -22,7 +22,7 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-            for i in self.word:
+            for i in range(len(self.word)):
                 if self.word[i] == guess:
                     self.word_guessed[i] = guess
             self.num_letters -= 1
