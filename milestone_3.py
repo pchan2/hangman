d55e40607ba10ad617a47a645f9da5ca0ff5ecff
Patrick Chan <q_p18@hotmail.com>
2023-09-20 16:11:06 +0100

    Tidy code

--- milestone_3.py
+++ milestone_3.py
@@ -4,14 +4,12 @@ import random
 
 def check_guess(guess):
     guess = guess.lower()
-
     if guess in word:
         print(f"Good guess! {guess} is in the word.")
     else:
         print(f"Sorry, {guess} is not in the word. Try again.")
 
 def ask_for_input():
-
     while True:
         guess = input('Guess a letter: ')
         if len(guess) == 1 and isalpha(guess):
