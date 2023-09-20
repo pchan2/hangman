3fc171a1e5c4eeecd6d723fc7ee7c73652d9a222
Patrick Chan <q_p18@hotmail.com>
2023-09-20 16:06:14 +0100

    Task 3: Create functions to run the checks.

--- milestone_3.py
+++ milestone_3.py
@@ -2,22 +2,28 @@ from curses.ascii import isalpha
 
 import random
 
+def check_guess(guess):
+    guess = guess.lower()
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+
+    while True:
+        guess = input('Guess a letter: ')
+        if len(guess) == 1 and isalpha(guess):
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    
+    check_guess(guess)
+
 word_list = ['banana', 'grapes', 'watermelon', 'blueberry', 'carambola']
 print(word_list)
 
 word = random.choice(word_list)
 print(word)
-
-guess = ''
-
-while True:
-    guess = input('Guess a letter: ')
-    if len(guess) and isalpha(guess):
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+ask_for_input()
